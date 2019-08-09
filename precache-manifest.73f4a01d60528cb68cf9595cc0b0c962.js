3a569a408c2d09e33372e31e777ce55428d063e3
SashaPlavi <sasha.bingulac@gmail.com>
2019-08-09 15:57:49 +0200

    Updates

---
+++ precache-manifest.73f4a01d60528cb68cf9595cc0b0c962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2f077fd999d37eb50f41ccdd4358f3",
+    "url": "/bit-blog-sasha/index.html"
+  },
+  {
+    "revision": "57df3cb436c5d73a8190",
+    "url": "/bit-blog-sasha/static/css/main.b5e9680b.chunk.css"
+  },
+  {
+    "revision": "ede14426d1874b88906e",
+    "url": "/bit-blog-sasha/static/js/2.b9083c5c.chunk.js"
+  },
+  {
+    "revision": "57df3cb436c5d73a8190",
+    "url": "/bit-blog-sasha/static/js/main.9dac8c67.chunk.js"
+  },
+  {
+    "revision": "b9fa5a042dd89728f2b8",
+    "url": "/bit-blog-sasha/static/js/runtime~main.be59d0fb.js"
+  }
+]);
